f81f93c5884da34b27cbe8073e83206d6cd0a579
sswarnakar <sswarnakar@hawk.iit.edu>
2017-10-23 23:26:41 -0500

    as3

--- AS3/src/HarrisCornerDetection.py
+++ AS3/src/HarrisCornerDetection.py
@@ -73,51 +73,6 @@ def HarrisCorner(image,window_size,hCons,threshold,sigmaX,sigmaY):
     return output_img,BetterCorners
 
 
-def get_descriptors(image, filtered_coords, wid=5):  # return pixel value
-
-    desc = []
-    for coords in filtered_coords:
-        patch = image[coords[0] - wid:coords[0] + wid + 1,
-                coords[1] - wid:coords[1] + wid + 1].flatten()
-        desc.append(patch)
-
-    return desc
-
-
-def match(desc1, desc2,threshold=0.5):
-
-    n = len(desc1[0])
-
-    d = -np.ones((len(desc1), len(desc2)))
-    for i in range(len(desc1)):
-        for j in range(len(desc2)):
-            d1 = (desc1[i] - np.mean(desc1[i])) / np.std(desc1[i])
-            d2 = (desc2[j] - np.mean(desc2[j])) / np.std(desc2[j])
-            mul=[a * b for a, b in zip(d1, d2)]
-            ncc_value = sum(mul) / (n - 1)
-            if ncc_value > threshold:
-                d[i, j] = ncc_value
-
-    ndx = np.argsort(-d)
-    matchscores = ndx[:, 0]
-
-    return matchscores
-
-
-def match_twosided(desc1, desc2, threshold=0.5):
-
-    matches_12 = match(desc1, desc2, threshold)
-    matches_21 = match(desc2, desc1, threshold)
-
-    ndx_12 = np.where(matches_12 >= 0)[0]
-
-    for n in ndx_12:
-        if matches_21[matches_12[n]] != n:
-            matches_12[n] = -1
-
-    return matches_12
-
-
 
 if __name__ == '__main__':
 
@@ -138,14 +93,6 @@ if __name__ == '__main__':
 
     cv2.imwrite("finalImg1.png", finalImg1)
     cv2.imwrite("finalImg2.png", finalImg2)
-
-    d1=get_descriptors(gray1,cornerList1)
-    d2=get_descriptors(gray2,cornerList2)
-
-    d1 = [x for x in d1 if x != []]
-    d2 = [x for x in d2 if x != []]
-
-    matches = match_twosided(d1, d2)
 
     orb = cv2.ORB_create()
 
